a18d89b081f3b6b8c5f55fe5760e9107bcf9fa54
Marcel <kuehn.marcel-it25@it.dhbw-ravensburg.de>
2026-02-13 11:03:43 +0100

    Section

---
+++ courses/coding-in-C/Lab_5/Section_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+    int x = 10;
+    int *p = &x;
+    int **pp = &p;
+    **pp = 42;
+
+    // TODO: complete the output statements
+    printf("x = %d\n", x);
+    printf("x via p = %d\n", *p);
+    printf("x via pp = %d\n", **pp);
+
+    return 0;
+}
+
+
+/*
+    x = int
+    p = int 
+    p* = int pointer 
+    pp = int 
+    *pp = int pointer 
+    **pp = int pointer pointer 
+
+*/
